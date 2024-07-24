327a1ae369f85640bad6cc08abf86aca321eddc0
Ratnaraj Anand <anand.ratnaraj@gmail.com>
2024-07-24 18:44:05 +0530

    installation and mysql connection setup done

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 require('dotenv').config()
 const express = require("express")
+require('./models/index')
+
 const app = express()
 const port = process.env.PORT || 3000
+
+
+
+app.use(express.json())
+
+app.get('/', (req, res) =>{
+    res.send("hello ratnaraj")
+})
 
 app.listen(port , ()=> {
     console.log("server is running at port " + `${port}`)
